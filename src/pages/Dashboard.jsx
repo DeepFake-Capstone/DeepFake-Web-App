fbb0ac9036b73de19545270daa090e3fcae6be32
Arshitsharma <arshitsharma2020@gmail.com>
2024-08-20 01:02:10 +0530

    developer pics added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,59 +6,59 @@ import { BackgroundBeams } from '../components/ui/background-beams'
 const people = [
     {
       id: 1,
-      name: "Arshit Sharma",
-      designation: "Software Engineer",
+      name: "Dr. Suresh Raikwar",
+      designation: "Mentor",
       image:
-        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+        "./src/assets/developer/dr_suresh.png",
     },
     {
       id: 2,
-      name: "Aseem Mittal",
-      designation: "Product Manager",
+      name: "Dr. Prashant S Rana",
+      designation: "Mentor",
       image:
-        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        "./src/assets/developer/dr_ps.png",
     },
     {
       id: 3,
-      name: "Himanshu Bansal",
-      designation: "Data Scientist",
+      name: "Arshit Sharma",
+      designation: "Software Engineer",
       image:
-        "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        "./src/assets/developer/arshit.png",
     },
     {
       id: 4,
-      name: "Mehak Sharma",
-      designation: "Product Manager",
+      name: "Himanshu Bansal",
+      designation: "Data Scientist",
       image:
-        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        "./src/assets/developer/himanshu.png",
     },
     {
       id: 5,
-      name: "Mukul Jindal",
-      designation: "Data Scientist",
+      name: "Mehak Sharma",
+      designation: "Product Manager",
       image:
-        "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        "./src/assets/developer/mehak.png",
     },
     {
       id: 6,
-      name: "Dr.Suresh Raikwar",
-      designation: "Product Manager",
+      name: "Mukul Jindal",
+      designation: "Data Scientist",
       image:
-        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        "./src/assets/developer/mukul.png",
     },
     {
       id: 7,
-      name: "Dr.Ps Rana",
-      designation: "Data Scientist",
+      name: "Aseem Mittal",
+      designation: "Product Manager",
       image:
-        "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        "./src/assets/developer/aseem.png",
     },
   ];
 
 export const Dashboard=()=>{
   return (
     <>
-      <BackgroundBeams className='bg-black fixed inset-0 w-screen h-screen -z-10 bg-black bg-cover' />
+      <BackgroundBeams className='bg-black fixed inset-0 w-screen h-screen -z-10 bg-cover' />
       <Header />
       <FileUploadSection />
       <DeveloperSection people={people} />
