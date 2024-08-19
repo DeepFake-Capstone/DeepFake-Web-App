95cc04eac272b93f69a1b140b412d85fec5ef8d1
Arshitsharma <arshitsharma2020@gamil.com>
2024-08-20 01:13:29 +0530

    adding data

--- src/components/DeveloperSection.jsx
+++ src/components/DeveloperSection.jsx
@@ -6,7 +6,7 @@ export const DeveloperSection = ({ people }) => {
     <div className="flex flex-row items-center justify-end mb-10 w-full px-20">
       <div className='flex flex-col'>
         <p className="z-10 text-2xl text-white text-sm mb-2 pr-4">Developers:</p>
-        <div className='flex flex-align'>
+        <div className='flex flex-align px-2'>
           <AnimatedTooltip items={people} />
         </div>
       </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,58 +2,7 @@ import { Header } from '../components/Header';
 import { FileUploadSection } from '../components/FileUploadSection';
 import { DeveloperSection } from '../components/DeveloperSection';
 import { BackgroundBeams } from '../components/ui/background-beams'
-
-const people = [
-    {
-      id: 1,
-      name: "Dr. Suresh Raikwar",
-      designation: "Mentor",
-      image:
-        "./src/assets/developer/dr_suresh.png",
-    },
-    {
-      id: 2,
-      name: "Dr. Prashant S Rana",
-      designation: "Mentor",
-      image:
-        "./src/assets/developer/dr_ps.png",
-    },
-    {
-      id: 3,
-      name: "Arshit Sharma",
-      designation: "Software Engineer",
-      image:
-        "./src/assets/developer/arshit.png",
-    },
-    {
-      id: 4,
-      name: "Himanshu Bansal",
-      designation: "Data Scientist",
-      image:
-        "./src/assets/developer/himanshu.png",
-    },
-    {
-      id: 5,
-      name: "Mehak Sharma",
-      designation: "Product Manager",
-      image:
-        "./src/assets/developer/mehak.png",
-    },
-    {
-      id: 6,
-      name: "Mukul Jindal",
-      designation: "Data Scientist",
-      image:
-        "./src/assets/developer/mukul.png",
-    },
-    {
-      id: 7,
-      name: "Aseem Mittal",
-      designation: "Product Manager",
-      image:
-        "./src/assets/developer/aseem.png",
-    },
-  ];
+import { people } from '../data/developerData';
 
 export const Dashboard=()=>{
   return (
