8665e1cdea7716d29f49a2232e016e8d717cd6dc
Arshitsharma <arshitsharma2020@gamil.com>
2024-08-20 01:16:55 +0530

    text prop added

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,7 @@
-export const Button = () => {
+export const Button = ({text}) => {
   return (
     <button className="px-4 py-2 rounded-md border border-neutral-300 bg-neutral-100 text-neutral-500 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md mt-8">
-      Hit API!!
+      {text}
     </button>
   );
 };
