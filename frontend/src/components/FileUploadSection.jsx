ac6a28d9d18b53b4a0054ae5ef2a15843b4f325a
Mukul-Jindal <Mukul-Jindal@github.com>
2024-08-22 01:46:54 +0530

    Implemented the working of ml model on frontend

--- frontend/src/components/FileUploadSection.jsx
+++ frontend/src/components/FileUploadSection.jsx
@@ -11,7 +11,7 @@ export const FileUploadSection = () => {
           <FileUpload />
           </div>
         </div>
-        <Button label={"hit api !!"}/>
+        {/* <Button label={"hit api !!"}/> */}
       </div>
     </div>
   );
