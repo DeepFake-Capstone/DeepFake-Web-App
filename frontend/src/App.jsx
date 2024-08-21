6f7331678522a2b64a2b0abad650214f3a136788
Arshitsharma <arshitsharma2020@gmail.com>
2024-08-21 11:26:41 +0530

    sidebar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import{
+import {
   BrowserRouter,
   Routes,
   Route,
-}from "react-router-dom";
+} from "react-router-dom";
 
-import {Dashboard} from './pages/Dashboard';
-import {Signin} from './pages/Signin';
-import {Signup} from './pages/Signup';
+import { Dashboard } from './pages/Dashboard';
+import { Signin } from './pages/Signin';
+import { Signup } from './pages/Signup';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Dashboard/>} />
-      <Route path='/signin' element={<Signin/>} />
-      <Route path='/signup' element={<Signup/>} />
-      <Route path='/Dashboard' element={<Dashboard/>} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
