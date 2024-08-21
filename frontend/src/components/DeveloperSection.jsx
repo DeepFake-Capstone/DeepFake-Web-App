82c69b044eb2c84c0f881e11a1cf50eacddc7f30
Arshitsharma <arshitsharma2020@gmail.com>
2024-08-21 12:12:51 +0530

    made responsive

--- frontend/src/components/DeveloperSection.jsx
+++ frontend/src/components/DeveloperSection.jsx
@@ -3,10 +3,10 @@ import { AnimatedTooltip } from './ui/animated-tooltip';
 
 export const DeveloperSection = ({ people }) => {
   return (
-    <div className="flex flex-row items-center justify-end mb-10 w-full px-20">
+    <div className="flex flex-row items-center justify-end mb-6 w-full px-10">
       <div className='flex flex-col'>
         <p className="z-10 text-2xl text-white text-sm mb-2 pr-4">Developers:</p>
-        <div className='flex flex-align px-2'>
+        <div className='flex flex-align px-2 mx-4'>
           <AnimatedTooltip items={people} />
         </div>
       </div>
