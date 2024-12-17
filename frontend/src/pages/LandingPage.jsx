1c82f77d18cd3a19afef0d9ed3a457bc0e827457
Mukul Jindal <mukul20jindal58@gmail.com>
2024-12-17 15:05:16 +0530

    Landing page exprimental results section done

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -26,52 +26,51 @@ export const LandingPage = () => {
                     </p>
                 </Vortex>
             </div>
-            <div className="bg-black">
 
-                {/* Developers card section */}
+            {/* Developers card section */}
+            <div className="bg-black">
                 <AnimatedTestimonials testimonials={people} />;
             </div>
 
             {/* section talking about the accuracy and efficency of the model */}
             <div className="w-[calc(100%)] bg-black overflow-hidden">
 
-                <div className="w-[calc(100%)] w-3/4 mx-auto bg-green-500  overflow-hidden">
-                    <h1 className="text-white">Experimental Results</h1>
+                <div className="w-3/4 mx-auto bg-black  overflow-hidden">
+                    <h1 className="text-white text-5xl my-5 font-bold">Experimental Results</h1>
 
                     {/* We need to replace the text here */}
-                    <p className="text-white">
+                    <p className="text-white my-5 text-xl">
                         We have performed extensive training and hyperparameter tuning, such as comparing different EfficientNet models, number of convolution layers, weights, data augmentations, dropout rates, and regularizers. In the end, the following settings give us the best results:
                     </p>
                     <div className="text-white">
                         <ul>
                             {modelEfficiency.map((item, index) => (
-                                <li key={index}> <BsCheck style={{ display: "inline" }} /> {item}</li>
+                                <li key={index} className="text-white my-1 text-lg" > <BsCheck style={{ display: "inline" }} /> {item}</li>
                             ))}
                         </ul>
                     </div>
 
+
                     {/* Accuracy Graphs */}
 
-                    <div className="flex flex-row">
+                    <div className="flex flex-row my-10">
                         {/* Images ki css set krni hai */}
 
-                        <MagicCard className=" w-1/4 mx-auto h-96">
+                        <MagicCard className=" w-2/5 mx-auto h-96">
                             <img
                                 src="src\assets\model_metrices\Training_Val_loss.jpg"
-
-                                className="h-full w-full rounded-3xl object-cover object-center"
+                                className="h-full w-full object-contain object-center"
                                 draggable={false}
                             />
                         </MagicCard>
-                        <MagicCard className=" w-1/4 mx-auto h-96">
+                        <MagicCard className=" w-2/5 mx-auto h-96">
                             <img
                                 src="src\assets\model_metrices\Training_val_Accuracy.jpg"
-                                className="h-full w-full rounded-3xl object-cover object-fit"
+                                className="h-full w-full object-contain object-center"
                                 draggable={false}
                             />
                         </MagicCard>
                     </div>
-
                 </div>
             </div>
         </>
